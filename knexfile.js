cd5b97fd546ee0531c55c058d671cbc99f1ff518
Eddy Caldas <eddycaldas@live.com>
2019-05-07 11:26:30 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgresql://localhost/today',
+    connection: 'postgres://localhost/today',
   },
   production: {
     client: 'pg',
