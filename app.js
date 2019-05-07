a32ac28aaa5272d376858fe2fc587b4adf53d722
Eddy Caldas <eddycaldas@live.com>
2019-05-07 11:22:53 -0600

    updates

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
  
 const routes = {
   article: require('./routes/article')
 }
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.use(cors())
 app.use('/article', routes.article);
 
