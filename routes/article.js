961855336c9c60797c6777a1d30971f1ad020c07
Eddy Caldas <eddycaldas@live.com>
2019-05-07 11:31:48 -0600

    update article.js with path

--- routes/article.js
+++ routes/article.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const queries = require('../db/queries');
+const path = require('path')
 
 router.get('/', (req, res) => {
-  queries.article.getAll()
+  queries
+    .article 
+    .getAll()
   .then(allInfo => {
     res.json(allInfo)
   })
